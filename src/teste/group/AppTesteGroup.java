357412228f7cfa97a8c1fd601d146ddd7bb525cc
dan7sc <su7cheng@gmail.com>
2021-06-05 14:36:40 -0300

    Add window with group layout

---
+++ src/teste/group/AppTesteGroup.java
@@ -0,0 +1,11 @@
+package teste.group;
+
+public class AppTesteGroup {
+  public static String DIRETORIO =
+    AppTesteGroup.class.getResource(".").getPath();
+
+  public static void main(String[] args) {
+    new Group(DIRETORIO + "dinov2.png");
+    // new Group(DIRETORIO + "peixe.gif");
+  }
+}
